3b8f5699ad9847f5d33fe0791e22fd73422dc350
theot <theotim@outlook.fr>
2021-07-22 14:26:11 +0200

    add travis

---
+++ backend/website/travis.py
@@ -0,0 +1,14 @@
+from website.settings import *
+
+SECRET_KEY="ezfezf'zefeer'((')==ù'((-è"
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': ''
+        }
+}
