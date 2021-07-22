f60fc45319c9d8e14ce4bb73cd1cc8e6fa59cbb7
theot <theotim@outlook.fr>
2021-07-22 14:32:19 +0200

    test travis

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -7,10 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
