fb8418552ff32ca2420ab953a1b9ce60ee7691bb
theodrem <theotim@outlook.fr>
2021-07-26 20:48:20 +0000

    update repo

--- backend/website/var.py
+++
@@ -1,5 +0,0 @@
-ENV = {
-    "SECRET_KEY": "dzdadkefzzer'ffsccfggoooioefzfejsknkscnssdcndkcndcdkfijfioppppjggrru-(ééée'",
-    "PASSWORD_DB":"Intelligence97",
-    "ALLOWED_HOSTS":"159.65.56.183"}
-
